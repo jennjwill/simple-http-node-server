a39912e3ed7fc53cd854f6ca3d28811febd04c16
Jenn J Will <jennijwill@gmail.com>
2019-07-22 13:17:48 -0700

    simple node server completed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const http = "http";
+const http = require("http");
 const port = 3000;
+const fs = require("fs");
 
 const requestHandler = (request, response) => {
   response.end(`Handling a request on port ${port}`);
@@ -15,7 +16,7 @@ server.listen(port, err => {
   console.log("server is listening on ${port}");
 });
 
-false.writeFile("hello-world.txt", "Hello to this great world", "utf8", err => {
+fs.writeFile("hello-world.txt", "Hello to this great world", "utf8", err => {
   if (err) throw err;
 
   console.log("success");
