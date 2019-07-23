50dc0cd25576fff070c26f6d87bf800fe29e27ca
Jenn J Will <jennijwill@gmail.com>
2019-07-23 12:39:46 -0700

    port string fixed and fs writeFile moved

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ const fs = require("fs");
 
 const requestHandler = (request, response) => {
   response.end(`Handling a request on port ${port}`);
+  fs.writeFile("hello-world.txt", "Hello to this great world", "utf8", err => {
+    if (err) throw err;
+
+    console.log("success");
+  });
 };
 
 const server = http.createServer(requestHandler);
 
 server.listen(port, err => {
   if (err) {
-    return console.log("You have an error: ${err}");
+    return console.log(`You have an error: ${err}`);
   }
 
-  console.log("server is listening on ${port}");
-});
-
-fs.writeFile("hello-world.txt", "Hello to this great world", "utf8", err => {
-  if (err) throw err;
-
-  console.log("success");
+  console.log(`server is listening on ${port}`);
 });
